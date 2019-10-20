ae0560ca71cca818555c19dabcbd282a1e949c99
Lewis Williams <LewisWilliams@abcdeMacBook.local>
2019-10-20 14:15:04 +0100

    Added func

--- src/data_types.rs
+++ src/data_types.rs
@@ -1,3 +1,11 @@
-pub fn run() {
+pub fn run(test: &str) {
+  println!("Hello");
+  let string = String::from(test);
+  println!("{}",string);
+}
 
+pub fn add(num1: i8, num2: i8) {
+  let addition: i8 = num1 + num2;
+  println!("{} + {} = {}",num1,num2,addition);
+  run("From the add function!!");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@
 //mod vars;
 mod data_types;
 fn main() {
-    data_types::run()
+
+    data_types::run("Vari");
+    data_types::add(3,6);
 }
