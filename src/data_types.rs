9532dfe3607dcf1547205523d8335b5d99eb638a
Lewis Williams <LewisWilliams@abcdeMacBook.local>
2019-10-20 19:31:09 +0100

    Updated README with new config image

--- src/data_types.rs
+++ src/data_types.rs
@@ -9,3 +9,17 @@ pub fn add(num1: i8, num2: i8) {
   println!("{} + {} = {}",num1,num2,addition);
   run("From the add function!!");
 }
+
+pub fn cond() {
+  let age: i8 = 19;
+  if age <= 20 {
+    println!("You are younger than 20");
+  } else {
+    println!("You are older than 20");
+  }
+}
+
+//Function that returns something
+pub fn returnNumber(number: i8) -> i8 {
+  return number;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-//mod print;
-//mod vars;
+mod print;
+mod vars;
 mod data_types;
 fn main() {
 
     data_types::run("Vari");
     data_types::add(3,6);
     data_types::cond();
+    println!(" {}", data_types::returnNumber(3))
 }
